4e3efe21f772f3dbe52e213ce3235f2305bfc936
bmoon <>
2018-08-11 19:06:32 +0900

    modified

--- ui/form.py
+++ ui/form.py
@@ -25,22 +25,31 @@ except AttributeError:
 class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName(_fromUtf8("Form"))
-        Form.resize(400, 300)
-        self.pushButton = QtGui.QPushButton(Form)
-        self.pushButton.setGeometry(QtCore.QRect(150, 120, 97, 27))
-        self.pushButton.setObjectName(_fromUtf8("pushButton"))
-        self.label = QtGui.QLabel(Form)
-        self.label.setGeometry(QtCore.QRect(170, 180, 66, 17))
-        self.label.setObjectName(_fromUtf8("label"))
+        Form.resize(677, 640)
+        self.listView = QtGui.QListView(Form)
+        self.listView.setGeometry(QtCore.QRect(20, 20, 621, 231))
+        self.listView.setObjectName(_fromUtf8("listView"))
+        self.pushButton_2 = QtGui.QPushButton(Form)
+        self.pushButton_2.setGeometry(QtCore.QRect(240, 260, 171, 27))
+        self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
+        self.listView_2 = QtGui.QListView(Form)
+        self.listView_2.setGeometry(QtCore.QRect(20, 340, 621, 241))
+        self.listView_2.setObjectName(_fromUtf8("listView_2"))
+        self.pushButton_4 = QtGui.QPushButton(Form)
+        self.pushButton_4.setGeometry(QtCore.QRect(240, 290, 171, 27))
+        self.pushButton_4.setObjectName(_fromUtf8("pushButton_4"))
 
         self.retranslateUi(Form)
-        QtCore.QObject.connect(self.pushButton, QtCore.SIGNAL(_fromUtf8("clicked()")), self.changeText)
+        QtCore.QObject.connect(self.pushButton_2, QtCore.SIGNAL(_fromUtf8("clicked()")), button2Clicked)
+        QtCore.QObject.connect(self.pushButton_4, QtCore.SIGNAL(_fromUtf8("clicked()")), button4Clicked)
         QtCore.QMetaObject.connectSlotsByName(Form)
-    def changeText(self):
-        self.label.setText("Hello.")
-
-    def retranslateUi(self, Form):
+    def button2Clicked(self,Form):
+        pass
+    def button4Clicked(self,Form):
+        pass
+ 
+   def retranslateUi(self, Form):
         Form.setWindowTitle(_translate("Form", "Form", None))
-        self.pushButton.setText(_translate("Form", "PushButton", None))
-        self.label.setText(_translate("Form", "TextLabel", None))
+        self.pushButton_2.setText(_translate("Form", "Add new account", None))
+        self.pushButton_4.setText(_translate("Form", "Delete this account", None))
 
